63a5b84217f49a4992509582ae661006f5de9132
00015897 <rayhona05@bk.ru>
2026-02-28 13:53:24 +0300

    Add docstring to blog forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,4 @@
+"""Forms for Post creation/editing and user registration."""
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
